5152bfdba08f3c47a306692751fb4c822ce0b007
jkr <abduqahhor.muhamadjonov@gmail.com>
2019-05-22 16:11:21 +0500

    Calendar appointments app with jQuery and Haml

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,20 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments = Appointment.order(appointment_time: :ASC)
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+
+    # we need to send latest set of Records in our response from
+    # the action
+    @appointments = Appointment.order(appointment_time: :ASC)
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:title, :appointment_time)
   end
 end
